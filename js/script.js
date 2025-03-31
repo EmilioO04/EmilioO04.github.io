6ea591025fef4ee08c31ce9ccd2302f4f28c23a5
EmilioO04 <157161047+EmilioO04@users.noreply.github.com>
2025-03-31 13:08:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,79 @@
-let personajes = [];
-
-document.getElementById("form-personaje").addEventListener("submit", function(e) {
-  e.preventDefault();
-  const personaje = {
-    nombre: document.getElementById("nombre").value,
-    rol: document.getElementById("rol").value,
-    universo: document.getElementById("universo").value,
-    poder: document.getElementById("poder").value,
-    tipoPoder: document.getElementById("tipo_poder").value,
-    debilidad: document.getElementById("debilidad").value
+let personajes = [
+  {
+    nombre: "Naruto Uzumaki",
+    rol: "Protagonista",
+    universo: "Naruto",
+    poder: "Rasengan",
+    tipoPoder: "Chakra",
+    debilidad: "No tiene un control completo sobre el Chakra",
+    tipoDebilidad: "Inexperiencia",
+    puntosPoder: 85,
+    nivelPoder: 8
+  },
+  {
+    nombre: "Sasuke Uchiha",
+    rol: "Antagonista",
+    universo: "Naruto",
+    poder: "Chidori",
+    tipoPoder: "Chakra",
+    debilidad: "Emociones incontroladas",
+    tipoDebilidad: "Impulsividad",
+    puntosPoder: 90,
+    nivelPoder: 9
+  },
+  {
+    nombre: "Sakura Haruno",
+    rol: "Aliada",
+    universo: "Naruto",
+    poder: "Fuerza sobrehumana",
+    tipoPoder: "Chakra",
+    debilidad: "Baja resistencia a ataques de largo alcance",
+    tipoDebilidad: "Física",
+    puntosPoder: 70,
+    nivelPoder: 7
+  },
+  {
+    nombre: "Kakashi Hatake",
+    rol: "Mentor",
+    universo: "Naruto",
+    poder: "Sharingan",
+    tipoPoder: "Jutsu",
+    debilidad: "Exceso de uso del Sharingan",
+    tipoDebilidad: "Energía",
+    puntosPoder: 95,
+    nivelPoder: 10
+  },
+  {
+    nombre: "Hinata Hyuga",
+    rol: "Aliada",
+    universo: "Naruto",
+    poder: "Byakugan",
+    tipoPoder: "Chakra",
+    debilidad: "Baja agresividad en combate",
+    tipoDebilidad: "Psicológica",
+    puntosPoder: 75,
+    nivelPoder: 7
+  },
+  {
+    nombre: "Itachi Uchiha",
+    rol: "Antagonista",
+    universo: "Naruto",
+    poder: "Tsukuyomi",
+    tipoPoder: "Jutsu",
+    debilidad: "Usar demasiado el Mangekyo Sharingan",
+    tipoDebilidad: "Físico",
+    puntosPoder: 98,
+    nivelPoder: 10
+  },
+  {
+    nombre: "Jiraiya",
+    rol: "Mentor",
+    universo: "Naruto",
+    poder: "Jutsu de invocación (como el sapo Gamabunta)",
+    tipoPoder: "Jutsu",
+    debilidad: "Vulnerabilidad al ataque físico directo",
+    tipoDebilidad: "Física",
+    puntosPoder: 85,
+    nivelPoder: 9
+  }
+];
